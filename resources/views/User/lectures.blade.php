f7cf1c7ec05e8b5e79b92418ee29b1588a75ea1f
Meqdad <abo.ateleh@gmail.com>
2020-10-19 12:19:23 +0400

    form12

--- resources/views/User/lectures.blade.php
+++ resources/views/User/lectures.blade.php
@@ -35,6 +35,8 @@
                 <div class="container">
                     <iframe id="iframe" class="responsive-iframe" src="{{$data->uri}}" ></iframe>
                 </div>
+                <div style="position: absolute;font-size: 45px;top: 92px;padding: 20px;transform: rotate(90deg);color:#6dd6bc8f">{{auth()->user()->first_name}}</div>
+                <div style="position: absolute;font-size: 45px;top: 45%;padding: 20px;transform: rotate(90deg);right: 100px;color: #6dd6bc8f">{{auth()->user()->first_name}}</div>
             </div>
         </div>
         <div class="row">
@@ -50,9 +52,9 @@
         <div class="row" style="text-align: center">
             <div class="col-md-12 socal_media_icons" style="padding: 50px 10px">
                 <img src="{{asset("img/socialmedia/instagram.png")}}" class="mmm" style="width: 30px">
-                <img src="{{asset("img/socialmedia/facebook.png")}}" style="width: 30px" class="fab fa-facebook"></img>
-                <img src="{{asset("img/socialmedia/twitter.png")}}" style="width: 30px" class="fab fa-twitter"></img>
-                <img src="{{asset("img/socialmedia/whatsapp.png")}}" style="width: 30px" class="fab fa-whatsapp"></img>
+                <img src="{{asset("img/socialmedia/facebook.png")}}" style="width: 30px" class="fab fa-facebook">
+                <img src="{{asset("img/socialmedia/twitter.png")}}" style="width: 30px" class="fab fa-twitter">
+                <img src="{{asset("img/socialmedia/whatsapp.png")}}" style="width: 30px" class="fab fa-whatsapp">
             </div>
         </div>
         <div class="row">
